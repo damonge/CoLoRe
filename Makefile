5423a7f0ac3e9e762384c8ba163b7f95abd6158e
damonge <dam_phys@gmail.com>
2016-07-21 13:44:16 +0100

    removed HPIX for the time being

--- Makefile
+++ Makefile
@@ -43,9 +43,6 @@ HDF5_LIB =
 #libconfig
 CONF_INC =
 CONF_LIB =
-#HEALPix
-HPIX_INC=
-HPIX_LIB=
 #
 ########## End of user-definable ##########
 
@@ -87,8 +84,8 @@ endif #SINGLE_PRECISION
 
 OPTIONS += $(DEFINEFLAGS)
 
-INC_ALL = -I./src $(GSL_INC) $(FFTW_INC) $(FITS_INC) $(HDF5_INC) $(CONF_INC) $(HPIX_INC)
-LIB_ALL = $(GSL_LIB) $(FFTW_LIB) $(FITS_LIB) $(HDF5_LIB) $(CONF_LIB) $(HPIX_LIB) -lconfig -lgsl -lgslcblas $(LIB_FFTW) -lchealpix
+INC_ALL = -I./src $(GSL_INC) $(FFTW_INC) $(FITS_INC) $(HDF5_INC) $(CONF_INC)
+LIB_ALL = $(GSL_LIB) $(FFTW_LIB) $(FITS_LIB) $(HDF5_LIB) $(CONF_LIB) -lconfig -lgsl -lgslcblas $(LIB_FFTW)
 ifeq ($(strip $(USE_HDF5)),yes)
 DEFINEFLAGS += -D_HAVE_HDF5
 LIB_ALL += -lhdf5 -lhdf5_hl -lz
@@ -105,9 +102,8 @@ COSMOO = src/cosmo.o
 FOURIERO = src/fourier.o
 GRIDO = src/grid_tools.o
 IOO = src/io.o
-HPIXO = src/healpix_extra.o
 MAIN = src/main.c
-OFILES = $(COMMONO) $(COSMOMADO) $(COSMOO) $(FOURIERO) $(GRIDO) $(IOO) $(HPIXO)
+OFILES = $(COMMONO) $(COSMOMADO) $(COSMOO) $(FOURIERO) $(GRIDO) $(IOO)
 
 EXEC = CoLoRe
 
--- src/common.h
+++ src/common.h
@@ -120,17 +120,6 @@ typedef struct {
   float dz_rsd; //RSD contribution
 } Gal;
 
-#ifndef _OLD_IM
-typedef struct {
-  int nz;
-  flouble *r0_arr;
-  flouble *rf_arr;
-  long **list_ipix;
-  int *num_pix;
-  flouble **maps;
-} OnionInfo;
-#endif //_OLD_IM
-
 typedef struct {
   char fnamePk[256];
   double OmegaM;
@@ -214,10 +203,6 @@ int rng_poisson(double lambda,gsl_rng *rng);
 void rng_delta_gauss(double *module,double *phase,
 		     gsl_rng *rng,double sigma2);
 void end_rng(gsl_rng *rng);
-void alloc_onion_info(ParamCoLoRe *par,OnionInfo *oi,int nside,
-		      int nz,flouble *z0_arr,flouble *zf_arr,
-		      int add_rsd,int add_next);
-void free_onion_info(OnionInfo *oi);
 
 
 //////
@@ -251,59 +236,5 @@ void end_fftw(void);
 //////
 // Functions defined in grid_tools.c
 void get_sources(ParamCoLoRe *par);
-
-//////
-// Defined in healpix_extra.c
-void he_write_healpix_map(flouble **tmap,int nfields,long nside,char *fname);
-flouble *he_read_healpix_map(char *fname,long *nside,int nfield);
-int he_ring_num(long nside,double z);
-long *he_query_strip(long nside,double theta1,double theta2,long *npix_strip);
-void he_ring2nest_inplace(flouble *map_in,long nside);
-void he_nest2ring_inplace(flouble *map_in,long nside);
-void he_udgrade(flouble *map_in,long nside_in,flouble *map_out,long nside_out,int nest);
-#ifdef _WITH_SHT
-#define HE_MAX_SHT 32
-#define HE_FWHM2SIGMA 0.00012352884853326381 //Transforms FWHM in arcmin to sigma_G in rad:
-long he_nalms(int lmax);
-long he_indexlm(int l,int m,int lmax);
-void he_alm2map(int nside,int lmax,int ntrans,flouble **maps,fcomplex **alms);
-void he_map2alm(int nside,int lmax,int ntrans,flouble **maps,fcomplex **alms);
-void he_alm2cl(fcomplex **alms_1,fcomplex **alms_2,
-	       int nmaps_1,int nmaps_2,int pol_1,int pol_2,flouble **cls,int lmax);
-void he_anafast(flouble **maps_1,flouble **maps_2,
-		int nmaps_1,int nmaps_2,int pol_1,int pol_2,
-		flouble **cls,int nside,int lmax);
-flouble *he_generate_beam_window(int lmax,flouble fwhm_amin);
-void he_alter_alm(int lmax,flouble fwhm_amin,fcomplex *alm_in,
-		  fcomplex *alm_out,flouble *window);
-flouble *he_synfast(flouble *cl,int nside,int lmax,unsigned int seed);
-//HE_NT
-#ifdef _WITH_NEEDLET
-#define HE_NBAND_NX 512
-#define HE_NORM_FT 2.2522836206907617
-#define HE_NL_INTPREC 1E-6
-#define HE_NT_NSIDE_MIN 32
-typedef struct {
-  double b;
-  double inv_b;
-  gsl_spline *b_spline;
-  gsl_interp_accel *b_intacc;
-  int nside0;
-  int nj;
-  int *nside_arr;
-  int *lmax_arr;
-  flouble **b_arr;
-} HE_nt_param;
-void he_nt_end(HE_nt_param *par);
-HE_nt_param *he_nt_init(flouble b_nt,int nside0);
-flouble ***he_alloc_needlet(HE_nt_param *par,int pol);
-void he_free_needlet(HE_nt_param *par,int pol,flouble ***nt);
-void he_nt_get_window(HE_nt_param *par,int j,flouble *b);
-fcomplex **he_map2needlet(HE_nt_param *par,flouble **map,flouble ***nt,
-			  int return_alm,int pol,int qu_in,int qu_out);
-fcomplex **he_needlet2map(HE_nt_param *par,flouble **map,flouble ***nt,
-			  int return_alm,int pol,int qu_in,int qu_out);
-#endif //_WITH_NEEDLET
-#endif //_WITH_SHT
 
 #endif //_COMMON_
